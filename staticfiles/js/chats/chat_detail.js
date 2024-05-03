21cb29b620b3f103da4f32006b19cb112f2c35ef
Shiyang-Zhao <shiyang2575326696@gmail.com>
2024-05-03 17:03:01 +0800

    date format

---
+++ staticfiles/js/chats/chat_detail.js
@@ -0,0 +1,57 @@
+import { establishWebSocket, sendMessage } from "./websocket.js";
+
+const showDetailPlaceholder = (chatPk) => {
+    document.querySelectorAll('.chat-detail-placeholder').forEach(placeholder => {
+        placeholder.style.display = 'none';
+    });
+
+    const detailPlaceholder = document.getElementById(`chat-detail-placeholder-${chatPk}`);
+    if (detailPlaceholder) {
+        detailPlaceholder.style.display = 'block';
+        establishWebSocket(chatPk);
+
+        const messageInput = detailPlaceholder.querySelector("#messageInput");
+        const sendButton = detailPlaceholder.querySelector("#sendButton");
+
+
+        sendButton.addEventListener('click', () => {
+            if (messageInput.value.trim() !== '') {
+                sendMessage({ content: messageInput.value.trim() });
+                messageInput.value = '';
+            } else {
+                alert("fuck you")
+            }
+        });
+
+        messageInput.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter' && !event.shiftKey) {
+                event.preventDefault();
+                if (messageInput.value.trim() !== '') { // Check if the form is valid
+                    sendMessage({ content: messageInput.value.trim() });
+                    messageInput.value = '';
+                } else {
+                    alert("You can't send an empty message");
+                }
+            }
+        });
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const chatPattern = /^\/chats\/chat\/\d+\/$/;
+    const currentPath = window.location.pathname;
+    if (chatPattern.test(currentPath)) {
+        const chatPk = currentPath.match(/\d+/)[0];
+        showDetailPlaceholder(chatPk);
+    }
+
+    document.querySelectorAll('.list-group-item').forEach(item => {
+        item.addEventListener('click', () => {
+            const chatPk = item.getAttribute('data-chat-pk');
+            showDetailPlaceholder(chatPk);
+            const url = item.getAttribute("data-href");
+            window.history.pushState(null, "", url);
+        });
+    });
+});
+
