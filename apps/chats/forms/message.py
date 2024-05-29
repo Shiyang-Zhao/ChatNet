2ced8cd647255fd74ffa43af70caddd0bba31dc7
Shiyang-Zhao <shiyang2575326696@gmail.com>
2024-05-29 19:02:45 +0800

    UI optimization

--- apps/chats/forms/message.py
+++
@@ -1,8 +0,0 @@
-# from django import forms
-# from ..models.message import Message
-
-
-# class MessageCreateForm(forms.ModelForm):
-#     class Meta:
-#         model = Message
-#         fields = ["content", "attachment"]
