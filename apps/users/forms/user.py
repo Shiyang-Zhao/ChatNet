538c504dc7c08454e3699e88b84d32b644dc46c3
Shiyang-Zhao <shiyang2575326696@gmail.com>
2024-06-25 15:39:17 +0800

    Story UI is more like Instagram

--- apps/users/forms/user.py
+++ apps/users/forms/user.py
@@ -15,9 +15,39 @@ class UserSignUpForm(UserCreationForm):
             "password2",
         ]
 
+    def __init__(self, *args, **kwargs):
+        super(UserSignUpForm, self).__init__(*args, **kwargs)
+        self.fields["username"].label = ""
+        self.fields["username"].widget.attrs.update(
+            {"placeholder": "Username", "autocomplete": "username"}
+        )
+        self.fields["email"].label = ""
+        self.fields["email"].widget.attrs.update(
+            {"placeholder": "Email", "autocomplete": "email"}
+        )
+        self.fields["password1"].label = ""
+        self.fields["password1"].widget.attrs.update(
+            {"placeholder": "Password", "autocomplete": "new-password"}
+        )
+        self.fields["password2"].label = ""
+        self.fields["password2"].widget.attrs.update(
+            {"placeholder": "Confirm Password", "autocomplete": "new-password"}
+        )
+
 
 class UserLoginForm(AuthenticationForm):
     remember_me = forms.BooleanField(required=False, label="Remember me")
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields["username"].label = ""
+        self.fields["username"].widget.attrs.update(
+            {"placeholder": "Username", "autocomplete": "username"}
+        )
+        self.fields["password"].label = ""
+        self.fields["password"].widget.attrs.update(
+            {"placeholder": "Password", "autocomplete": "current-password"}
+        )
 
 
 class UserUpdateForm(forms.ModelForm):
